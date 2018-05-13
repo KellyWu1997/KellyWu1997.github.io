56170d6d0af8702908866bd63c2236961269b72f
Yi Wu <y456wu@uwaterloo.ca>
2018-05-13 17:25:33 -0400

    modify designer page

--- js/index.js
+++ js/index.js
@@ -1,11 +1,18 @@
 function showImg(){
-    console.log("run");
-    
     $('.code-img').css('display','block');
     $('.code-img').animate({ "left": "-=220px" }, "1000" );
     setTimeout(function (){
         $('.design-img').css('display','block');
         $('.design-img').animate({ "left": "+=220px" }, "1000" );
       }, 500);
-  
 }
+
+
+
+$(document).ready ( function () {
+
+   $('.design-img1 img').animate({left: '50px'}, 1000);
+    $('.design-img2 img').animate({right: '50px'}, 1000);
+    $('.design-img3 img').animate({left: '50px'}, 1000);
+    $('.design-img4 img').animate({right: '50px'}, 1000);
+});
